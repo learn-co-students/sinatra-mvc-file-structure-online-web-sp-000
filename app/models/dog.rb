7ee5e6ae770fa8486c49f9b2bcfb0b5a780b404c
James Lenahan <james.k.lenahan@gmail.com>
2020-02-06 18:08:34 +0000

    Done.

---
+++ app/models/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  attr_accessor :name, :age, :breed
+  
+  @@dogs = []
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    @@dogs << self
+  end
+  
+  def self.all
+    @@dogs
+  end
+end
