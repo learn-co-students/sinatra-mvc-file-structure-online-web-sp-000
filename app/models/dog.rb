87f9fe746a9fa0b58cdefcf2ce6be90c46747aff
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-05-15 13:19:39 +0000

    Done.

---
+++ app/models/dog.rb
@@ -0,0 +1,16 @@
+class Dog 
+  
+  attr_accessor :name, :breed, :age 
+  
+  def initialize(name, breed, age)
+    @name = name 
+    @breed = breed 
+    @age = age 
+  end 
+  
+  @@all = [] 
+  
+  def self.all 
+    @@all << self 
+  end 
+end 
