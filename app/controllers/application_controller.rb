de147c80460bd645c88150519ab36e865662956b
ting682 <tchung682@gmail.com>
2020-07-06 15:51:31 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
-class ApplicationController < Sinatra::Base
 
+
+class ApplicationController < Sinatra::Base
+  
   configure do
   	set :views, "app/views"
   	set :public_dir, "public"
   end
 
   get "/" do
+   # binding.pry
   	erb :index
   end
+
+
 end
+
+
